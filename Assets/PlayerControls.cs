9715b64867426e4fe5cb58b352f284bf34c83fcf
KohlerCoding <mikkel_eske@outlook.dk>
2017-09-27 18:39:39 +0200

    initial commit

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.V;
+    public KeyCode moveRight = KeyCode.B;
+    
+    public float speed = 8.0f;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(moveUp))
+        {
+            var vel = rb2d.velocity;
+            vel.y = speed;
+            rb2d.velocity = vel;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            var vel = rb2d.velocity;
+            vel.y = -1 * speed;
+            rb2d.velocity = vel;
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            var vel = rb2d.velocity;
+            vel.x = speed;
+            rb2d.velocity = vel;
+        }
+        else if (Input.GetKey(moveRight))
+        {
+            var vel = rb2d.velocity;
+            vel.x = -1 * speed;
+            rb2d.velocity = vel;
+        }
+        else if (!Input.anyKey)
+        {
+            var vel = rb2d.velocity;
+            vel.y = 0.0f;
+            rb2d.velocity = vel;
+        }
+
+        var reset = rb2d.velocity;
+        reset.x = 0;
+        rb2d.velocity = reset;
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    GameObject[] pauseObjects;
+
+    Transform theBall;
+    
+    // Use this for initialization
+    void Start () {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (Time.timeScale == 0)
+            {
+                Debug.Log("high");
+                Time.timeScale = 1;
+                hidePaused();
+            }
+            else
+            {
+                Time.timeScale = 0;
+                showPaused();
+            }
+        }
+    }
+
+    void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void pauseControl()
+    {
+        if(Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        } else if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    public void showPaused()
+    {
+        foreach(GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+    public void hidePaused()
+    {
+        foreach(GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+}
